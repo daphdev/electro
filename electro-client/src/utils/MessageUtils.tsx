721dc3ce9b23f54b8aee64183e75ebc01fbece82
Dang <60851390+markiusphan@users.noreply.github.com>
2022-05-29 04:42:32 +0700

    Update MessageUtils.tsx

--- electro-client/src/utils/MessageUtils.tsx
+++ electro-client/src/utils/MessageUtils.tsx
@@ -1,4 +1,4 @@
 export default class MessageUtils {
-  static max = (subject: string, value: number) => `${subject} có ít nhất ${value} ký tự`;
-  static min = (subject: string, value: number) => `${subject} chỉ có nhiều nhất ${value} ký tự`;
+  static min = (subject: string, value: number) => `${subject} có ít nhất ${value} ký tự`;
+  static max = (subject: string, value: number) => `${subject} chỉ có nhiều nhất ${value} ký tự`;
 }
