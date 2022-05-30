1cd117931fa6f0c3a9a64f3431a98a452f00be8c
Dang <60851390+markiusphan@users.noreply.github.com>
2022-05-30 18:52:25 +0700

    Update SearchFields.java

--- electro-server/src/main/java/com/electro/constant/SearchFields.java
+++ electro-server/src/main/java/com/electro/constant/SearchFields.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface SearchFields {
     List<String> PROVINCE = List.of("name", "code");
-    List<String> DISTRICT = List.of("name", "code");
+    List<String> DISTRICT = List.of("name", "code", "province.name", "province.code");
     List<String> ADDRESS = List.of("line", "province.name", "province.code", "district.name", "district.code");
     List<String> USER = List.of("username", "fullname", "email", "phone",
             "address.province.name", "address.province.code", "address.district.name", "address.district.code");
