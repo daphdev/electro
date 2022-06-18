b16fc2414bc411b98815edb80d0a96bc3213df73
Dang <60851390+markiusphan@users.noreply.github.com>
2022-06-19 02:00:23 +0700

    Update index.ts

--- electro-client/src/pages/province/index.ts
+++ electro-client/src/pages/province/index.ts
@@ -1,5 +1,5 @@
 import ProvinceManage from 'pages/province/ProvinceManage';
-import ProvinceCreate from './ProvinceCreate';
-import ProvinceUpdate from './ProvinceUpdate';
+import ProvinceCreate from 'pages/province/ProvinceCreate';
+import ProvinceUpdate from 'pages/province/ProvinceUpdate';
 
 export { ProvinceManage as default, ProvinceCreate, ProvinceUpdate };
