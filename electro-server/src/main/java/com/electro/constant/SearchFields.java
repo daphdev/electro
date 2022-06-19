89f77a0839424f26b7231c0b6dc4b1bb96c40cab
Dang <60851390+markiusphan@users.noreply.github.com>
2022-06-19 18:27:19 +0700

    Update SearchFields.java

--- electro-server/src/main/java/com/electro/constant/SearchFields.java
+++ electro-server/src/main/java/com/electro/constant/SearchFields.java
@@ -12,20 +12,22 @@ public interface SearchFields {
             "address.district.name", "address.district.code");
     List<String> ROLE = List.of("code", "name");
     List<String> BRAND = List.of("name", "code", "description");
-    List<String> OFFICE = List.of("name", "address.province.name", "address.province.code",
+    List<String> OFFICE = List.of("name", "address.line",
+            "address.province.name", "address.province.code",
             "address.district.name", "address.district.code");
     List<String> DEPARTMENT = List.of("name", "code");
     List<String> JOB_TITLE = List.of("name", "code");
     List<String> JOB_LEVEL = List.of("name", "code");
     List<String> JOB_TYPE = List.of("name", "code");
     List<String> EMPLOYEE = List.of("user.username", "user.fullname", "user.email", "user.phone",
+            "user.address.line",
             "user.address.province.name", "user.address.province.code",
             "user.address.district.name", "user.address.district.code", "office.name");
     List<String> CUSTOMER_GROUP = List.of("name", "code", "description", "color", "status");
     List<String> CUSTOMER_RESOURCE = List.of("name", "code", "description", "color", "status");
     List<String> CUSTOMER_STATUS = List.of("name", "code", "description", "color", "status");
-
-    List<String> CUSTOMER = List.of("user.id", "user.username", "user.fullname", "user.email", "user.phone",
-            "user.address.province.name", "user.address.province.code", "user.address.district.name",
-            "user.address.district.code");
+    List<String> CUSTOMER = List.of("user.username", "user.fullname", "user.email", "user.phone",
+            "user.address.line",
+            "user.address.province.name", "user.address.province.code",
+            "user.address.district.name", "user.address.district.code");
 }
