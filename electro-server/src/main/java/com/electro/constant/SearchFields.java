eeefadac0321db4712a4310ed9bee40eed1d5145
Dang <60851390+markiusphan@users.noreply.github.com>
2022-06-19 18:24:53 +0700

    Update SearchFields.java

--- electro-server/src/main/java/com/electro/constant/SearchFields.java
+++ electro-server/src/main/java/com/electro/constant/SearchFields.java
@@ -7,7 +7,9 @@ public interface SearchFields {
     List<String> DISTRICT = List.of("name", "code", "province.name", "province.code");
     List<String> ADDRESS = List.of("line", "province.name", "province.code", "district.name", "district.code");
     List<String> USER = List.of("username", "fullname", "email", "phone",
-            "address.province.name", "address.province.code", "address.district.name", "address.district.code");
+            "address.line",
+            "address.province.name", "address.province.code",
+            "address.district.name", "address.district.code");
     List<String> ROLE = List.of("code", "name");
     List<String> BRAND = List.of("name", "code", "description");
     List<String> OFFICE = List.of("name", "address.province.name", "address.province.code",
